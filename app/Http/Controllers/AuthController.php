a0d19e8522f96c80b2f1a25af199d05d51edad9b
Yudha <48747021+Trisnayudha@users.noreply.github.com>
2023-05-15 11:00:22 +0700

    update token

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -7,6 +7,8 @@ use App\Models\Company;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\Rule;
 use Tymon\JWTAuth\JWTAuth;
 
 class AuthController extends Controller
@@ -20,24 +22,39 @@ class AuthController extends Controller
 
     public function loginOtp(Request $request)
     {
-        $this->validate($request, [
-            'email' => 'required|email|exists:company,email',
-        ]);
+        $validate = Validator::make(
+            $request->all(),
+            [
+                'email' => ['required', 'email', 'exists:company,email'],
+            ],
+            [
+                'email.required' => 'Email wajib diisi',
+                'email.exists' => 'Email Not Found'
+            ]
+        );
+        if ($validate->fails()) {
+            $data = [
+                'email' => $validate->errors()->first('email'),
+            ];
+            $response['status'] = 401;
+            $response['message'] = 'Invalid credentials';
+            $response['payload'] = $data;
+        } else {
 
-        $email = $request->input('email');
-        $otp = rand(100000, 999999);
-        // Save the OTP to the database or cache with an expiration time
-        // Here we are using cache for storing the OTP
-        Cache::put("otp_$email", $otp, 300); // Store the OTP for 5 minutes
+            $email = $request->input('email');
+            $otp = rand(100000, 999999);
+            // Save the OTP to the database or cache with an expiration time
+            // Here we are using cache for storing the OTP
+            Cache::put("otp_$email", $otp, 300); // Store the OTP for 5 minutes
 
-        // Send OTP to the user's email
-        Mail::to($email)->send(new OtpMail($otp));
+            // Send OTP to the user's email
+            Mail::to($email)->send(new OtpMail($otp));
 
-        return response()->json([
-            'status' => 200,
-            'message' => 'OTP has been sent to your email',
-            'payload' => []
-        ]);
+            $response['status'] = 200;
+            $response['message'] = 'OTP has been sent to your email';
+            $response['payload'] = [];
+        }
+        return response()->json($response);
     }
 
     public function verifyOtp(Request $request)
@@ -72,58 +89,74 @@ class AuthController extends Controller
                 'status' => 400,
                 'message' => 'Invalid OTP, please try again',
                 'payload' => []
-            ]);
+            ], 400);
         }
     }
 
     public function loginEmailPassword(Request $request)
     {
-        $this->validate($request, [
-            'email' => 'required|email',
-            'password' => 'required'
-        ]);
-
-        $credentials = $request->only('email', 'password');
-
-        try {
-            if (!$token = $this->jwt->attempt($credentials)) {
+        $validate = Validator::make(
+            $request->all(),
+            [
+                'email' => ['required', 'email', 'exists:company,email'],
+            ],
+            [
+                'email.required' => 'Email wajib diisi',
+                'email.exists' => 'Email Not Found'
+            ]
+        );
+        if ($validate->fails()) {
+            $data = [
+                'email' => $validate->errors()->first('email'),
+                'password' => ''
+            ];
+            $response['status'] = 401;
+            $response['message'] = 'Invalid credentials';
+            $response['payload'] = $data;
+        } else {
+            $credentials = $request->only('email', 'password');
+            try {
+                if (!$token = $this->jwt->attempt($credentials)) {
+                    $data = [
+                        'email' => '',
+                        'password' => 'Password wrong'
+                    ];
+                    return response()->json([
+                        'status' => 401,
+                        'message' => 'Invalid credentials',
+                        'payload' => $data
+                    ], 401);
+                }
+            } catch (\Tymon\JWTAuth\Exceptions\TokenExpiredException $e) {
                 return response()->json([
-                    'status' => 401,
-                    'message' => 'Invalid credentials',
+                    'status' => 500,
+                    'message' => 'Token expired',
                     'payload' => []
-                ], 401);
+                ], 500);
+            } catch (\Tymon\JWTAuth\Exceptions\TokenInvalidException $e) {
+                return response()->json([
+                    'status' => 500,
+                    'message' => 'Token invalid',
+                    'payload' => []
+                ], 500);
+            } catch (\Tymon\JWTAuth\Exceptions\JWTException $e) {
+                return response()->json([
+                    'status' => 500,
+                    'message' => $e->getMessage(),
+                    'payload' => []
+                ], 500);
             }
-        } catch (\Tymon\JWTAuth\Exceptions\TokenExpiredException $e) {
-            return response()->json([
-                'status' => 500,
-                'message' => 'Token expired',
-                'payload' => []
-            ], 500);
-        } catch (\Tymon\JWTAuth\Exceptions\TokenInvalidException $e) {
-            return response()->json([
-                'status' => 500,
-                'message' => 'Token invalid',
-                'payload' => []
-            ], 500);
-        } catch (\Tymon\JWTAuth\Exceptions\JWTException $e) {
-            return response()->json([
-                'status' => 500,
-                'message' => $e->getMessage(),
-                'payload' => []
-            ], 500);
+            // Get the authenticated user
+            $user = $this->jwt->user();
+
+            $response['status'] = 200;
+            $response['message'] = 'Successfully Login';
+            $response['payload'] = [
+                'user_id' => $user->id,
+                'token' => $token,
+                'pin' => $user->pin != null ? true : false,
+            ];
         }
-
-
-        // Get the authenticated user
-        $user = $this->jwt->user();
-
-        $response['status'] = 200;
-        $response['message'] = 'Successfully Login';
-        $response['payload'] = [
-            'user_id' => $user->id,
-            'token' => $token,
-            'pin' => $user->pin != null ? true : false,
-        ];
         return response()->json($response);
     }
 }
