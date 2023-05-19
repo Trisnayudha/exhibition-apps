75a967de44ac7508cb0e81fe5b4c2f6dd6fbb213
Yudha <48747021+Trisnayudha@users.noreply.github.com>
2023-05-19 16:28:45 +0700

    update new flow check date

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class TestController extends Controller
 {
@@ -18,11 +19,37 @@ class TestController extends Controller
 
     public function index(Request $request)
     {
+        // Menangkap tanggal dari request
+        $date = $request->date;
+
+        // Mencari data terakhir
+        $lastEntry = DB::table('events_date')->orderBy('created_at', 'desc')->first();
+
+        if (!empty($date)) {
+            // Memeriksa apakah sudah ada data dalam tabel
+            if ($lastEntry) {
+                // Jika ada, memperbarui entri terakhir
+                DB::table('events_date')
+                    ->where('id', $lastEntry->id)
+                    ->update(['date' => $date]);
+            } else {
+                // Jika tidak ada, memasukkan data baru
+                DB::table('events_date')->insert(['date' => $date]);
+            }
+
+            $message = 'Date has been updated/inserted';
+            $payloadDate = $date;
+        } else {
+            // Jika request->date kosong, menampilkan data terakhir
+            $message = 'Showing last date';
+            $payloadDate = $lastEntry ? $lastEntry->date : null;
+        }
+
         return response()->json([
             'status' => 200,
-            'message' => 'show date events',
+            'message' => $message,
             'payload' => [
-                'date' => $request->date
+                'date' => $payloadDate
             ]
         ]);
     }
