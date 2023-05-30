002fc07b6bc2101d65d72eb3c992023e71acf23c
Yudha <48747021+Trisnayudha@users.noreply.github.com>
2023-05-30 16:43:18 +0700

    questioner updated

--- app/Http/Controllers/QuestionerController.php
+++ app/Http/Controllers/QuestionerController.php
@@ -25,35 +25,83 @@ class QuestionerController extends Controller
         $questions = [
             [
                 'id' => 1,
-                'question' => 'What is your return policy?',
+                'question' => 'Pre-Show Information & Support?',
                 'optional' => 'PG',
+                'listPG' => ['Very Satisfied', 'Satisfied', 'Dissatisfied']
             ],
             [
                 'id' => 2,
-                'question' => 'How do I track my order?',
-                'optional' => 'Essay',
+                'question' => 'Exhibitor recognition – marketing/promotional materials',
+                'optional' => 'PG',
+                'listPG' => ['Very Satisfied', 'Satisfied', 'Dissatisfied']
             ],
             [
                 'id' => 3,
-                'question' => 'Can I cancel my order?',
+                'question' => 'How did the event/ exhibition floor layout',
                 'optional' => 'PG',
+                'listPG' => ['Very Satisfied', 'Satisfied', 'Dissatisfied']
+            ],
+            [
+                'id' => 4,
+                'question' => 'Event mobile app',
+                'optional' => 'PG',
+                'listPG' => ['Very Satisfied', 'Satisfied', 'Dissatisfied']
+            ],
+            [
+                'id' => 5,
+                'question' => 'How was your experience with the app?',
+                'optional' => 'PG',
+                "listPG" => ['I enjoyed it and found it useful', 'I used the app and positive about it', 'I used the app and experienced a couple of issues']
+            ],
+            [
+                'id' => 6,
+                'question' => 'Please rate your overall Indonesia Miner Conference &amp; Exhibition 2023
+                experience',
+                'optional' => 'PG',
+                'listPG' => ['Very Satisfied', 'Satisfied', 'Dissatisfied']
+            ],
+            [
+                'id' => 7,
+                'question' => 'Please share about your experience at this year’s show or suggestions on how we
+                can make the event better for you in the future:',
+                'optional' => 'Essay',
+                'listPG' => []
+            ],
+            [
+                'id' => 8,
+                'question' => 'How likely are you to participate in Indonesia Miner 2024?',
+                'optional' => 'PG',
+                'listPG' => ['Definitely', 'Most Likely', 'Undecided', 'Will Not Exhibiting']
+            ],
+            [
+                'id' => 9,
+                'question' => 'When would be the best time for you to receive the event prospectus/ kit prior to the
+                event date?',
+                'optional' => 'PG',
+                'listPG' => ['3 months', '6 months', '1 year']
+            ],
+            [
+                'id' => 10,
+                'question' => 'If you are not planning to exhibit, what is your primary reason?',
+                'optional' => 'Essay',
+                'listPG' => []
             ],
             // Add more questions here
         ];
 
-        // Generate random optional values for additional questions
-        $optionalValues = ['PG', 'Essay'];
+        // // Generate random optional values for additional questions
+        // $optionalValues = ['PG', 'Essay'];
 
-        for ($i = 4; $i <= 10; $i++) {
-            $randomIndex = array_rand($optionalValues);
-            $optional = $optionalValues[$randomIndex];
+        // for ($i = 4; $i <= 10; $i++) {
+        //     $randomIndex = array_rand($optionalValues);
+        //     $optional = $optionalValues[$randomIndex];
 
-            $questions[] = [
-                'id' => $i,
-                'question' => "Question $i",
-                'optional' => $optional,
-            ];
-        }
+        //     $questions[] = [
+        //         'id' => $i,
+        //         'question' => "Question $i",
+        //         'optional' => $optional,
+        //     ];
+        // }
 
         $response = [
             'status' => 200,
